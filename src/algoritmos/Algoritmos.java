d5c62a744227c868fbb00e516c099f765befdcc5
Aluno <Aluno@172.32.1.41>
2017-10-05 21:56:51 -0300

    Criação de função com retorno. Criação de um pacote com uma classe que possui o código da Aula 28.

--- src/algoritmos/Algoritmos.java
+++ src/algoritmos/Algoritmos.java
@@ -2,10 +2,24 @@ package algoritmos;
 
 public class Algoritmos {
 
-     public static void main(String[] args) {
-        
-         
-        
+    public static void quadrado(double n) {
+        double r = n * n;
+        System.out.println("Quadrado: " + r);
     }
-    
+
+    public static double metade(double v) {
+        double r = v / 2;
+        return r;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Raiz: " + Math.sqrt(15));
+
+        //System.out.println("Quadrado: "+ quadrado(15));
+        quadrado(15);
+
+        System.out.println("Metade: " + metade(15));
+    }
+
 }
